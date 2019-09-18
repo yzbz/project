fcf76c65922fc6740934dfa9e74298e04cd824b2
zhangpeng <523631013@qq.com>
2019-09-18 11:04:17 +0800

    add test tmpl

---
+++ Conf/db_config.php
@@ -0,0 +1,34 @@
+<?php
+if(!defined('IN_TP_COUPON')) exit();
+return array(
+"DB_TYPE"=> "mysql",
+"DB_HOST"=> "127.0.0.1",
+"DB_NAME"=>"zhenmuwang",
+"DB_USER"=>"jr1",
+"DB_PWD"=>"JRyhdweb123",
+"DB_PORT"=>"3306",
+"DB_PREFIX"=>"stpc_",
+
+    'DB_MMPLAN'=>array(
+        "DB_TYPE"=> "mysql",
+        "DB_HOST"=> "114.215.192.224",
+        "DB_NAME"=>"zhenmuwang",
+        "DB_USER"=>"jr1",
+        "DB_PWD"=>"JRyhdweb123",
+        "DB_PORT"=>"3306",
+        "DB_PREFIX"=>"mmp_",
+    ),
+
+    'DB_TP_COUPON'=>array(
+        "DB_TYPE"=> "mysql",
+        "DB_HOST"=> "114.215.192.224",
+        "DB_NAME"=>"zhenmuwang",
+        "DB_USER"=>"jr1",
+        "DB_PWD"=>"JRyhdweb123",
+        "DB_PORT"=>"3306",
+        "DB_PREFIX"=>"stpc_",
+    ),
+
+)
+
+?>
