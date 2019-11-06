aa73c9851901be2a46f11c870bd27c0ec1e8ecf5
zhangpeng <523631013@qq.com>
2019-11-06 15:45:05 +0800

    base version 1.0

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Disable Strict Host checking for non interactive git clones
-mkdir -p -m 0700 /root/.ssh
+#mkdir -p -m 0700 /root/.ssh
 #echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
 #if [ ! -z "$SSH_KEY" ]; then
@@ -10,13 +10,13 @@ mkdir -p -m 0700 /root/.ssh
 #fi
 
 # Setup git variables
-if [ ! -z "$GIT_EMAIL" ]; then
- git config --global user.email "$GIT_EMAIL"
-fi
-if [ ! -z "$GIT_NAME" ]; then
- git config --global user.name "$GIT_NAME"
- git config --global push.default simple
-fi
+#if [ ! -z "$GIT_EMAIL" ]; then
+# git config --global user.email "$GIT_EMAIL"
+#fi
+#if [ ! -z "$GIT_NAME" ]; then
+# git config --global user.name "$GIT_NAME"
+# git config --global push.default simple
+#fi
 
 # Pull down code form git for our site!
 #if [ ! -z "$GIT_REPO" ]; then
@@ -37,20 +37,20 @@ else
 fi
 
 # Very dirty hack to replace variables in code with ENVIRONMENT values
-if [[ "$TEMPLATE_NGINX_HTML" == "1" ]] ; then
-  for i in $(env)
-  do
-    variable=$(echo "$i" | cut -d'=' -f1)
-    value=$(echo "$i" | cut -d'=' -f2)
-    if [[ "$variable" != '%s' ]] ; then
-      replace='\$\$_'${variable}'_\$\$'
-      find /var/www/html -type f -exec sed -i -e 's/'${replace}'/'${value}'/g' {} \;
-    fi
-  done
-fi
+#if [[ "$TEMPLATE_NGINX_HTML" == "1" ]] ; then
+#  for i in $(env)
+#  do
+#    variable=$(echo "$i" | cut -d'=' -f1)
+#    value=$(echo "$i" | cut -d'=' -f2)
+#    if [[ "$variable" != '%s' ]] ; then
+#      replace='\$\$_'${variable}'_\$\$'
+#      find /var/www/html -type f -exec sed -i -e 's/'${replace}'/'${value}'/g' {} \;
+#    fi
+#  done
+#fi
 
 #render-templates.sh $WWWROOT/Conf $WWWROOT/Conf
-/render-templates.sh /etc/nginx/sites-available /etc/nginx/sites-enabled
+#/render-templates.sh /etc/nginx/sites-available /etc/nginx/sites-enabled
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
